e74a02c5fb0eaf4625bcf2f8c57c8d31a9371edd
hiepnguyen223 <hiepnguyenn223@gmail.com>
2022-04-14 13:11:59 +0700

    Add Mongoose Paginate

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
+import aggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const RoomSchema = new Schema(
     {
@@ -41,6 +42,7 @@ RoomSchema.plugin(require("mongoose-autopopulate"));
 
 // plugin for pagination
 RoomSchema.plugin(mongoosePaginate);
+RoomSchema.plugin(aggregatePaginate);
 
 const Room = model("rooms", RoomSchema);
 export default Room;
