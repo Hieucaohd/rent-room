dbe3335a107be3aebee29a42708f498636d8b1aa
hiepnguyen223 <hiepnguyenn223@gmail.com>
2022-04-25 09:33:28 +0700

    Add look up from owner collection

--- src/helpers/createMatchAggregation.js
+++ src/helpers/createMatchAggregation.js
@@ -6,38 +6,43 @@ export const createMatchAggregation = (conditions) => {
             ...(address.district && { 'home.district': address.district }),
             ...(address.ward && { 'home.ward': address.ward }),
         }),
-        ...(price && price.scope && {
-            price: {
-                $gte: price.scope.min,
-                $lte: price.scope.max
-            },
-        }),
+        ...(price &&
+            price.scope && {
+                price: {
+                    $gte: price.scope.min,
+                    $lte: price.scope.max,
+                },
+            }),
         ...(livingExpenses && {
-            ...(livingExpenses.electricityCondition && livingExpenses.electricityCondition.scope && {
-                'home.electricityPrice': {
-                    $gte: livingExpenses.electricityCondition.scope.min,
-                    $lte: livingExpenses.electricityCondition.scope.max,
+            ...(livingExpenses.electricityCondition &&
+                livingExpenses.electricityCondition.scope && {
+                    'home.electricityPrice': {
+                        $gte: livingExpenses.electricityCondition.scope.min,
+                        $lte: livingExpenses.electricityCondition.scope.max,
+                    },
+                }),
+            ...(livingExpenses.waterCondition &&
+                livingExpenses.waterCondition.scope && {
+                    'home.waterPrice': {
+                        $gte: livingExpenses.waterCondition.scope.min,
+                        $lte: livingExpenses.waterCondition.scope.max,
+                    },
+                }),
+        }),
+        ...(square &&
+            square.scope && {
+                square: {
+                    $gte: square.scope.min,
+                    $lte: square.scope.max,
                 },
             }),
-            ...(livingExpenses.waterCondition && livingExpenses.waterCondition.scope && {
-                'home.waterPrice': {
-                    $gte: livingExpenses.waterCondition.scope.min,
-                    $lte: livingExpenses.waterCondition.scope.max,
+        ...(floor &&
+            floor.scope && {
+                floor: {
+                    $gte: floor.scope.min,
+                    $lte: floor.scope.max,
                 },
             }),
-        }),
-        ...(square && square.scope && {
-            square: {
-                $gte: square.scope.min,
-                $lte: square.scope.max,
-            },
-        }),
-        ...(floor && floor.scope && {
-            floor: {
-                $gte: floor.scope.min,
-                $lte: floor.scope.max
-            },
-        }),
         ...(liveWithOwner && { 'home.liveWithOwner': liveWithOwner }),
     };
 };
