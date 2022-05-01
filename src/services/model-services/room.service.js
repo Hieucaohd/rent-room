3ed718b56b60a3ee21ec6ef6b9d973fb9384d5f0
Hieupc <71920934+Hieucaohd@users.noreply.github.com>
2022-05-01 20:03:23 +0600

    add min price and max price field in home.

--- src/services/model-services/room.service.js
+++ src/services/model-services/room.service.js
@@ -135,6 +135,26 @@ export class RoomService extends BaseService {
         );
     }
 
+    /**
+     * @param {String | ObjectId} homeId
+     * @param {RequestContext} context
+     * @returns {Promise<Number>}
+     */
+    static async getMinPriceInHome(homeId, context) {
+        const room = await Room.find({ home: homeId }).sort({ price: 1 }).limit(1);
+        return room.price;
+    }
+
+    /**
+     * @param {String | ObjectId} homeId
+     * @param {RequestContext} context
+     * @returns {Promise<Number>}
+     */
+    static async getMaxPriceInHome(homeId, context) {
+        const room = await Room.find({ home: homeId }).sort({ price: -1 }).limit(1);
+        return room.price;
+    }
+
     static async getInstanceById(id, context) {
         const room = await this.getRoomById(id, context);
         return room;
